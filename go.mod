c7f07b69173498a5f945f878c4c3f5ab09ebe339
Tamal Saha <tamal@appscode.com>
2021-03-01 03:36:34 -0800

    added-all

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module kmodules.xyz/catalog-checker
 
 go 1.16
 
-require github.com/codeskyblue/go-sh v0.0.0-20200712050446-30169cf553fe
+require (
+	github.com/codeskyblue/go-sh v0.0.0-20200712050446-30169cf553fe
+	kmodules.xyz/client-go v0.0.0-20210228131103-84674e27d4fe
+)
--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "github.com/codeskyblue/go-sh"
+import (
+	"fmt"
+	shell "github.com/codeskyblue/go-sh"
+	"kmodules.xyz/client-go/tools/parser"
+	"os"
+)
 
 func main() {
-	sh.Command("echo", "hello\tworld").Command("cut", "-f2").Run()
+	sh := shell.NewSession()
+	sh.SetDir(os.ExpandEnv("$HOME/go/src/kubedb.dev/installer"))
+	sh.ShowCMD = true
+
+
+	out, err := sh.Command("helm", "template", "charts/kubedb-catalog").Output()
+	if err != nil {
+		panic(err)
+	}
+	resources, err := parser.ListResources(out)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(len(resources))
 }
